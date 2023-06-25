148de013e284fa043f6c8c3483c8e0525dab880c
Enikuomehin Adejuwon <emirbadoo@gmail.com>
2023-06-25 22:34:31 +0100

    added widget import

--- lib/src/src.dart
+++ lib/src/src.dart
@@ -5,3 +5,5 @@ export 'impulse_utils.dart';
 export 'impulse_utils_method_channel.dart';
 export 'impulse_utils_platform_interface.dart';
 export 'models/application.dart';
+export 'models/file_size.dart';
+export 'widgets/media_thumbnail.dart';
